1563c8665494d5d93e1c7086290b11176f67cd2f
Ashley Wang <ashleyswang@google.com>
2020-08-07 19:35:52 -0700

    add server handlers for nb merge

--- jupyterlab_gitsync/jupyterlab_gitsync/tests/handlers_test.py
+++ jupyterlab_gitsync/jupyterlab_gitsync/tests/handlers_test.py
@@ -3,11 +3,29 @@ from unittest.mock import Mock, MagicMock, patch
 
 from jupyterlab_gitsync import handlers
 
+def create_test_app():
+  app = tornado.web.Application()
+  host_pattern = '.*$'
+  app.add_handlers(host_pattern, [
+    ('/sync', SyncHandler),
+    ('/setup', SetupHandler),
+    ('/nbinit', NotebookInitHandler),
+    ('/nbmerge', NotebookMergeHandler)
+  ])
+  return app
 
-class TestList(unittest.TestCase):
+class TestNotebookInitHandler(tornado.testing.AsyncHTTPTestCase):
 
-  def testList(self):
-    pass
+  def get_app(self): 
+    return create_test_app()
+
+  def test_notebook_init(self):
+    path = './test_files'
+    file_path = './test_files/init_cache_test.ipynb'
+
+    # TO DO (ashleyswang): complete full handler test
+
+class TestNotebookMergeHandler(tornado.testing.AsyncHTTPTestCase):
 
 if __name__ == '__main__':
   unittest.main()

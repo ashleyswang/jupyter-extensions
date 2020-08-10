c78427613d70b521d0d487bffcae456174f12632
Ashley Wang <ashleyswang@google.com>
2020-08-10 15:41:35 -0700

    mend

--- jupyterlab_gitsync/jupyterlab_gitsync/tests/handlers_test.py
+++ jupyterlab_gitsync/jupyterlab_gitsync/tests/handlers_test.py
@@ -3,30 +3,11 @@ from unittest.mock import Mock, MagicMock, patch
 
 from jupyterlab_gitsync import handlers
 
-def create_test_app():
-  app = tornado.web.Application()
-  host_pattern = '.*$'
-  app.add_handlers(host_pattern, [
-    ('/sync', SyncHandler),
-    ('/setup', SetupHandler),
-    ('/nbinit', NotebookInitHandler),
-    ('/nbmerge', NotebookMergeHandler)
-  ])
-  return app
 
-class TestNotebookInitHandler(tornado.testing.AsyncHTTPTestCase):
+class TestList(unittest.TestCase):
 
-  def get_app(self): 
-    return create_test_app()
-
-  # TO DO (ashleyswang): create test for handler full functionality
-
-class TestNotebookMergeHandler(tornado.testing.AsyncHTTPTestCase):
-
-  def get_app(self): 
-    return create_test_app()
-
-  # TO DO (ashleyswang): create test for handler full functionality
+  def testList(self):
+    pass
 
 if __name__ == '__main__':
   unittest.main()

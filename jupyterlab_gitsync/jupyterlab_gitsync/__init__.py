2bf3c473153ac45b5f98d16d9273fc505f296850
Ashley Wang <38049558+ashleyswang@users.noreply.github.com>
2020-08-11 13:17:11 -0700

    Add nbresolve handler for nbsync
    Co-authored-by: Ashley Wang <ashleyswang@google.com>

--- jupyterlab_gitsync/jupyterlab_gitsync/__init__.py
+++ jupyterlab_gitsync/jupyterlab_gitsync/__init__.py
@@ -25,5 +25,6 @@ def load_jupyter_server_extension(nb_server_app):
       (url_path_join(gcp_v1_endpoint, 'setup') + '(.*)', SetupHandler),
       (url_path_join(gcp_v1_endpoint, 'nbinit') + '(.*)', NotebookInitHandler),
       (url_path_join(gcp_v1_endpoint, 'nbmerge') + '(.*)', NotebookMergeHandler),
+      (url_path_join(gcp_v1_endpoint, 'nbresolve') + '(.*)', NotebookResolveHandler)
     ])
 
